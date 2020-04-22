8f4f56a9803939223b5a06f97e4da202e2cff5e2
t4cKt1ng <63959967+mR-H4cK-IT@users.noreply.github.com>
2020-04-22 10:29:24 +0300

    Add files via upload

---
+++ CcCalculator.py
@@ -0,0 +1,19 @@
+#python 2.7.15
+import time
+
+An = "Answer ="
+
+a = int(input("Enter first number cok"))
+b = int(input("Enter second number asu")) 
+c = a - b
+d = a + b
+print("Processing...")
+time.sleep(3)
+print(An,c)
+time.sleep(1)
+print("proses su...")
+time.sleep(3)
+print(An,d)
+time.sleep(1)
+print("If you like this PLS GIVE A STAR")
+print("       By PDAP")
